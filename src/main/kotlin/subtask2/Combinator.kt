b84a47ff01e0536e4c35c5a282827d4800e8e2f3
Yura <babitskiy.yura@gmail.com>
2020-05-03 15:27:23 +0300

    subtask 1,2

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -3,7 +3,22 @@ package subtask2
 class Combinator {
 
     // TODO: Complete the following function
-    fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+    fun factorial(num: Int): Long {
+        return if (num >= 1)
+            num * factorial(num - 1)
+        else
+            1
     }
+
+    fun checkChooseFromArray(array: Array<Int>): Int? {
+        val m = array[0]
+        val n = array[1]
+
+        for(x in 0..n){
+            if(factorial(x)*factorial(n-x) == factorial(n)/m)
+                return x
+        }
+        return null
+    }
+
 }
